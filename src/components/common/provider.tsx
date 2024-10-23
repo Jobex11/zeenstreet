af528287bac6de95e8a51d4de996663c0ed1a29b
nehemiahcode <codehubby@gmail.com>
2024-10-23 10:54:17 +0100

    fixing error in index.html file

--- src/components/common/provider.tsx
+++ src/components/common/provider.tsx
@@ -4,7 +4,6 @@
 import React from 'react'
 // import { Provider } from "react-redux"
 import { Toaster } from "sonner"
-import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 interface ProvideProps {
     children: React.ReactNode
@@ -14,12 +13,6 @@ export default function ReduxProvider({ children }: ProvideProps) {
     return (
         // <Provider store={store}>
         <>
-            <ProgressBar
-                height="2px"
-                color="#D25804"
-                options={{ showSpinner: false }}
-                shallowRouting
-            />
             <Toaster richColors expand={false} position='top-right' />
             {children}
         </>
