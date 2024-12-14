40c16ae944589a16083ffdb73ae1f41940531fc2
nehemiahcode <codehubby@gmail.com>
2024-12-14 22:58:01 +0100

    fixed deployment bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 // import path from "path"
 import react from "@vitejs/plugin-react"
+import path from "path";
 import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths";
+// import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   define: {
@@ -10,6 +11,15 @@ export default defineConfig({
   // optimizeDeps: {
   //   include: ['@telegram-apps/sdk-react'],
   // },
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      "@components": `${path.resolve(__dirname, "./src/components/")}`,
+      "@assets":`${path.resolve(__dirname, "./src/assets/")}`,
+      "@hooks":`${path.resolve(__dirname, "./src/hooks/")}`,
+      "@lib":`${path.resolve(__dirname, "./src/lib/")}`,
+    },
+  },
 
 })
