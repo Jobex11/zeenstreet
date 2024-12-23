b40cac8b056e4a1c55661b49ff030f86473e8bc1
Nehemiah Ekemezie <codehubby@gmail.com>
2024-12-23 14:45:31 +0100

    Update cards.ts

--- src/hooks/redux/cards.ts
+++ src/hooks/redux/cards.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const cardsApi = createApi({
     reducerPath: 'cardsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/api/cards" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://ravegenie-vgm7.onrender.com/api/cards" }),
     tagTypes: ['Cards'],
     endpoints: (builder) => ({
         getAllcards: builder.query({
